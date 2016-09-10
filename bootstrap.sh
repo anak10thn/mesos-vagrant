146af9d75a50e52e8c409609d809e606272d534f
anak10thn <anak10thn@gmail.com>
2016-09-11 06:15:05 +0700

    box update

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,7 @@
 #sudo rpm -Uvh http://repos.mesosphere.com/el/7/noarch/RPMS/mesosphere-el-repo-7-1.noarch.rpm --nodeps
 #sudo yum -y install mesos marathon
 #sudo yum -y install mesosphere-zookeeper
-sudo yum install -y epel-release net-tools psmisc htop tmux
+sudo yum install -y sshpass epel-release net-tools psmisc htop tmux
 sudo cat > /etc/hosts << EOF
 127.0.0.1   localhost localhost.localdomain localhost4 localhost4.localdomain4
 ::1         localhost localhost.localdomain localhost6 localhost6.localdomain6
@@ -14,3 +14,12 @@ EOF
 
 sudo sed -i "s|PasswordAuthentication no|PasswordAuthentication yes|g" /etc/ssh/sshd_config
 sudo service sshd restart
+su vagrant
+if [ ! -f $HOME/.ssh/id_rsa.pub ]
+then
+ssh-keygen -f $HOME/.ssh/id_rsa -t rsa -N ''
+fi
+sshpass -p "vagrant" ssh-copy-id -i $HOME/.ssh/id_rsa.pub vagrant@m
+sshpass -p "vagrant" ssh-copy-id -i $HOME/.ssh/id_rsa.pub vagrant@s1
+sshpass -p "vagrant" ssh-copy-id -i $HOME/.ssh/id_rsa.pub vagrant@s2
+sshpass -p "vagrant" ssh-copy-id -i $HOME/.ssh/id_rsa.pub vagrant@s3
